078d5894f29cb98b98fcaf6b58ff52ddf8456715
kobi-hershtiktec <kobi@hershtiktec.com>
2026-06-16 17:11:30 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/hershtiktec/', 
+    // מוגדר כסלאש בודד עבור דומיין אישי ראשי
+    base: '/', 
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
